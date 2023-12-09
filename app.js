a85d300000fdfb4eb827166a4088da7776c06289
Santiago <santiaguitobestoso@gmail.com>
2023-12-09 01:33:35 -0300

    delete src

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
-app.use('/api', require('../router/app.router'));
+app.use('/api', require('./router/app.router'));
 app.get('/', (req, res) => {
     res.send('Hola mundo');
 })
